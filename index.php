cee99b0c13c0a93c60dcf90f2e6a983f862ab46f
Aamirpal Sehmi <aamirpal@ucreate.co.in>
2019-12-27 14:45:14 +0530

    changes

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
+header('Content-Type: text/event-stream');
+header('X-Accel-Buffering: no');
+header('Cache-Contro: no-cache');
+header('Connection: keep-alive');
+header('Access-Control-Allow-Origin: *');
 
 while (true) {
-	header('Content-Type: text/event-stream');
-	header('X-Accel-Buffering: no');
-	header('Cache-Contro: no-cache');
-	header('Connection: keep-alive');
-	header('Access-Control-Allow-Origin: *');
 	
-	echo "ping...\n<br>";
+	print("ping...\n<br>");
 	ob_flush();
 	flush();
 	sleep(1);
