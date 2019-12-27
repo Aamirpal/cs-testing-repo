3885ee09c617f9f038171252dc7303a362bcb86f
Aamirpal Sehmi <aamirpal@ucreate.co.in>
2019-12-27 14:43:06 +0530

    add headers

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
 while (true) {
+	header('Content-Type: text/event-stream');
+	header('X-Accel-Buffering: no');
+	header('Cache-Contro: no-cache');
+	header('Connection: keep-alive');
+	header('Access-Control-Allow-Origin: *');
+	
 	echo "ping...\n<br>";
 	ob_flush();
 	flush();
