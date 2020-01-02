84387ebb5d486442d4e3d178eac22071552f375f
Aamirpal Sehmi <aamirpal@ucreate.co.in>
2020-01-02 18:05:55 +0530

    changes

--- app/Http/Controllers/TestingController.php
+++ app/Http/Controllers/TestingController.php
@@ -17,48 +17,19 @@ class TestingController extends Controller
 
     public function streamNotifications()
     {
-        session_write_close();
-        @set_time_limit(0); // Disable time limit
-        // Prevent buffering
-        if (function_exists('apache_setenv')) {
-            @apache_setenv('no-gzip', 1);
-        }
-        @ini_set('zlib.output_compression', 0);
-        @ini_set('implicit_flush', 1);
-        // while (ob_get_level() != 0) {
-        //     ob_end_flush();
-        // }
-        ob_implicit_flush(1);
-        /* ultility function for sending SSE messages */
-        header('Content-Type: text/event-stream, charset=UTF-8');
-        header('Cache-Control: no-cache,  must-revalidate');
-        //header('Transfer-encoding: chunked');
-        header('X-Accel-Buffering: no');
-        header('Connection: keep-alive');
-        while (ob_get_level()) ob_end_clean();
-        $counter = 1;
-        while (1) {
-            // Every second, send a "ping" event.
-            
+        if (ob_get_level() == 0) ob_start();
+        for ($i = 0; $i<10; $i++){
+
+            echo "<br> Line to show.";
+            echo str_pad('',4096)."\n";    
+
+            ob_flush();
             flush();
-            ob_get_contents();
-            $curDate = date("r h:i:sa");
-            print("id: {$counter}\n");
-            //print("event: ping\n");
-            print("data: The server time is: {$curDate}{$counter}\n\n");
-            //print('data: "time": "' . $curDate . '"');
-            //  print("\n\n");
-        
-            // Send a simple message at random intervals.
-            
-            flush();
-            if($counter < 50){
-                usleep(2 * 10000);
-            }else{
-                sleep(5);
-            }
-            
-            $counter++;
+            sleep(2);
         }
+
+        echo "Done.";
+
+        ob_end_flush();
     }
 }
